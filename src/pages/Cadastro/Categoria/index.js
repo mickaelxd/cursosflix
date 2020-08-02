23a2ee42e654d47615f603a95b0fedc290e4d449
Mickael Rocha <mickael.n.xd@gmail.com>
2020-08-02 14:16:15 -0300

    Eslint config, FormField props, useEffect config

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -1,76 +1,100 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
-function CadastroCategoria(){
-
+function CadastroCategoria() {
   const InitialValues = {
     name: '',
     description: '',
-  }
+  };
 
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(InitialValues);
 
-  function setValue(key, value){
+  function setValue(key, value) {
     setValues({
       ...values,
-      [key]: value
-    })
+      [key]: value,
+    });
   }
 
-  function handleChange(e){
+  function handleChange(e) {
     setValue(
       e.target.getAttribute('name'),
-      e.target.value
-    )
+      e.target.value,
+    );
   }
 
-  function handleSubmit(e){
+  useEffect(() => {
+    const URL_BACKEND = 'http://localhost:3333/categorias';
+    fetch(URL_BACKEND)
+      .then(async (response) => {
+        const data = await response.json();
+        setCategorias([
+          ...data,
+        ]);
+        console.log('Loading Complete : ', data);
+      });
+  }, []);
+
+  function handleSubmit(e) {
     e.preventDefault();
-    console.log('Adicionada a categoria: ', values )
-    setCategorias([...categorias, values])
-    setValues(InitialValues)
+
+    console.log('Adicionada a categoria: ', values);
+
+    setCategorias([...categorias, values]);
+    setValues(InitialValues);
   }
 
   return (
-      <PageDefault>
-        <h1>Cadastro de Categoria: {values.name}</h1>
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {' '}
+        {values.name}
+      </h1>
 
-        <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
 
-          <FormField 
-            label="Nome da Categoria"
-            type="text"
-            name="name"
-            value={values.name}
-            onChange={handleChange}
-          />
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+        />
 
-          <FormField 
-            label="Descrição"
-            type="text"
-            name="description"
-            value={values.description}
-            onChange={handleChange}
-          />
+        <FormField
+          label="Descrição"
+          type="text"
+          name="description"
+          value={values.description}
+          onChange={handleChange}
+        />
 
-          <button>
-            Cadastrar
-          </button>
+        <Button style={{ background: '#2b2b2b' }}>
+          Cadastrar
+        </Button>
 
-        </form>
+      </form>
 
-        <ul>
-          {categorias.map((categoria, index) => (
-            <li key={categoria + index}>
-              {categoria.name}
-            </li>
-          ))}
-        </ul>
-      </PageDefault>
-  )
+      {categorias.length === 0 && (
+      <div>
+        Loading...
+      </div>
+      )}
+
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={categoria.name}>
+            {categoria.name}
+          </li>
+        ))}
+      </ul>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 import dadosIniciais from '../../data/dados_iniciais.json';
 
@@ -7,17 +7,16 @@ import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
 
-
 function Home() {
-  //Peguei essa função de aleatorizar da Github/KellyTrindade, obrigado!
-  const categoria = Math.floor( ( Math.random() * dadosIniciais.categorias.length ) )
-  const video = Math.floor( ( Math.random() * dadosIniciais.categorias[categoria].videos.length ) )
+  // Peguei essa função de aleatorizar da Github/KellyTrindade, obrigado!
+  const categoria = Math.floor((Math.random() * dadosIniciais.categorias.length));
+  const video = Math.floor((Math.random() * dadosIniciais.categorias[categoria].videos.length));
 
   return (
-    <div style={{background: "#141414"}}>
+    <div style={{ background: '#141414' }}>
       <Menu />
 
-      <BannerMain 
+      <BannerMain
         videoTitle={dadosIniciais.categorias[categoria].videos[video].titulo}
         url={dadosIniciais.categorias[categoria].videos[video].url}
         videoDescription={dadosIniciais.categorias[categoria].videos[video].descricao}
@@ -25,28 +24,28 @@ function Home() {
       />
 
       <Carousel
-      ignoreFirstVideo
-      category={dadosIniciais.categorias[0]}
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
       />
 
       <Carousel
-      category={dadosIniciais.categorias[1]}
+        category={dadosIniciais.categorias[1]}
       />
 
       <Carousel
-      category={dadosIniciais.categorias[2]}
+        category={dadosIniciais.categorias[2]}
       />
 
       <Carousel
-      category={dadosIniciais.categorias[3]}
+        category={dadosIniciais.categorias[3]}
       />
 
       <Carousel
-      category={dadosIniciais.categorias[4]}
+        category={dadosIniciais.categorias[4]}
       />
 
       <Carousel
-      category={dadosIniciais.categorias[5]}
+        category={dadosIniciais.categorias[5]}
       />
 
       <Footer />
